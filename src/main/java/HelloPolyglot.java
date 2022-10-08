4b86774905ae2888d749130ac09cc9682dd70de9
Jedrzej Kalinowski <jkalinowski1@sii.pl>
2022-10-08 21:39:18 +0200

    name fixed and example polished

--- src/main/java/HelloPolyglot.java
+++ src/main/java/HelloPolyglot.java
@@ -1,7 +1,5 @@
-import org.graalvm.polyglot.*;
-
-import java.text.SimpleDateFormat;
-import java.time.format.DateTimeFormatter;
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Value;
 
 public class HelloPolyglot {
 
@@ -10,28 +8,29 @@ public class HelloPolyglot {
                 allowAllAccess(true).build();
 
         Value result = polyglotContext.eval("js",
-                "({ "                   +
-                        "id   : 42, "       +
-                        "text : '42', "     +
-                        "arr  : [1,42,3] "  +
-                        "})");
+                """
+                        ({
+                        id   : 2,
+                        text : 'Sii Get Digital',
+                        array  : ['Robert C. Martin','Bruce Eckel','Vlad Mihalcea', 'Jarosław Pałka'] 
+                        })
+                        """);
         assert result.hasMembers();
 
         int id = result.getMember("id").asInt();
-        assert id == 42;
+        assert id == 2;
 
         String text = result.getMember("text").asString();
-        assert text.equals("42");
+        assert text.equals("Sii Get Digital");
 
-        Value array = result.getMember("arr");
+        Value array = result.getMember("array");
         assert array.hasArrayElements();
-        assert array.getArraySize() == 3;
-        assert array.getArrayElement(1).asInt() == 42;
+        assert array.getArraySize() == 4;
+        assert array.getArrayElement(1).asString().equals("Robert C. Martin");
 
-//        Value array = polyglotContext.eval("js", "[1,2,42,4]");
-//        int result = array.getArrayElement(2).asInt();
-//        System.out.println(result);
-        System.out.println("JS array: " + array);
+        System.out.println("Conference number: " + id);
+        System.out.println("Conference name: " + text);
+        System.out.println("Star speakers: " + array);
     }
 
 }
--- src/main/js/polyglot.js
+++ src/main/js/polyglot.js
@@ -2,4 +2,4 @@ var df = new (Java.type("java.text.SimpleDateFormat"));
 var date = new (Java.type("java.util.Date"));
 
 console.log(date)
-console.log('Hi from SII Get Digital @ ' + df.format(date))
+console.log('Hi, we\'re at SII Get Digital on ' + df.format(date))
